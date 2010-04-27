257cb0265effd07d6e93eaa6a2d97778d34ac776
Yalcin ACIKYILDIZ <yalcin@webliyacelebi.com>
2010-04-27 14:31:15 +0300

    GNU/GPL License, README

--- lib/imdb.rb
+++ lib/imdb.rb
@@ -6,18 +6,3 @@ require "nokogiri"
 require "imdb/search"
 require "imdb/movie"
 require "imdb/person"
-
-#IMDB::Search.movie("fight club").each do
-#  |result|
-#  movie = IMDB::Movie.new(result.id)
-#  p movie.title
-#  movie.cast.each do
-#    |cast|
-#    p "#{cast.name} as #{cast.char}"
-#  end
-#  p movie.poster
-#end
-
-#movie = IMDB::Movie.new('0133093')
-#p movie.poster
-
